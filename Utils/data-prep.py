66b76add3cd16853d9574febd2f8030d54c27693
Neeraj-Natu <natu.neeraj@gmail.com>
2018-10-04 08:40:54 +0100

    adding in the data and the  bare bones files

--- Utils/data-prep.py
+++ Utils/data-prep.py
@@ -1,1 +1,15 @@
-# The paper mentions to prepare the data from S&P 500 although we would use Quandl to get our data.
+# The paper mentions to prepare the data from S&P 500 although 
+# This repository uses 10 popular stocks that have been around for 20 years instead of entire S&P 
+# to avoid complexity in preparing the data and avoid surviorship bias
+
+# We use the free Data from Alpha Vantage on below stocks 
+
+
+import requests
+import json
+
+
+url = "https://www.alphavantage.co/query"
+apikey = " V1SZPLE0U8CCXSFW."
+
+
