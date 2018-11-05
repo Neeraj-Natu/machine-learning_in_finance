015b1623146f4203ea6a61d79cfc25d779b9baeb
Neeraj-Natu <natu.neeraj@gmail.com>
2018-11-05 19:19:04 +0000

    adding in the changes for Random Forest model

---
+++ Random-Forest/Random-Forest_impl.py
@@ -0,0 +1,20 @@
+# Load scikit's random forest classifier library
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.externals import joblib
+
+# Load pandas
+import pandas as pd
+
+# Load numpy
+import numpy as np
+
+
+train_data = np.genfromtxt("..\data\Train_data_scaled.csv" , delimiter=",")  # Returns np.array
+train_labels = np.genfromtxt("..\data\Train_labels_scaled.csv" , delimiter=",") # Returns np.array
+
+
+model = RandomForestRegressor(n_estimators=250, max_depth=10);
+model.fit(train_data, train_labels);
+
+
+joblib.dump(model, '.\model\stock_prediction.sav');
