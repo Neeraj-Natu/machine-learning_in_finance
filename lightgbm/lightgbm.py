6b5eddd45a3e9ec1011f9c0b7891de05e7f7dfe4
Neeraj-Natu <natu.neeraj@gmail.com>
2018-11-06 23:40:58 +0000

    adding validation set with lightgbm model

--- lightgbm/lightgbm.py
+++ lightgbm/lightgbm.py
@@ -6,8 +6,11 @@ import numpy as np
 
 train_data = np.genfromtxt("..\data\Train_data_scaled.csv" , delimiter=",")  # Returns np.array
 train_labels = np.genfromtxt("..\data\Train_labels_scaled.csv" , delimiter=",") # Returns np.array
+eval_data = np.genfromtxt("..\data\Validation_data_scaled.csv" , delimiter=",")  # Returns np.array
+eval_labels = np.genfromtxt("..\data\Validation_labels_scaled.csv" , delimiter=",") # Returns np.array
 
 train_dataset = lgb.Dataset(train_data, label=train_labels);
+eval_dataset = lgb.Dataset(eval_data, label=eval_labels);
 
 params = {}
 params['learning_rate'] = 0.0003
@@ -18,6 +21,6 @@ params['num_leaves'] = 10
 params['min_data'] = 50
 params['max_depth'] = 5
 
-model = lgb.train(params, train_dataset, 250);
+model = lgb.train(params, train_dataset, 250, early_stopping_rounds=150, valid_sets=[eval_dataset],verbose_eval=20);
 
 model.save_model('.\model\stock_prediction.txt');
